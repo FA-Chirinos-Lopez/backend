8a3444cef0b65afbf39527974457c7f539339d88
FelixChirinosLopez <69871835+FA-Chirinos-Lopez@users.noreply.github.com>
2022-01-18 19:08:44 +0000

    18/01

---
+++ src/api/theme-color/routes/theme-color.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * theme-color router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::theme-color.theme-color');

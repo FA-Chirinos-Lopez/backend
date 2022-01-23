f873bb5bd74be72d7c134d6bcae68f8e40c55a96
FelixChirinosLopez <69871835+FA-Chirinos-Lopez@users.noreply.github.com>
2022-01-23 23:26:38 +0000

    2.3.1

--- src/api/theme-color/controllers/theme-color.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  theme-color controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::theme-color.theme-color');
--- src/api/theme-color/services/theme-color.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * theme-color service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::theme-color.theme-color');
